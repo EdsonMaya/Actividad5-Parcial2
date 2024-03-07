b7fb430f716b72dd84f8f7c69cabef43274bdf5e
Edson.maya <edson.maya@iest.edu.mx>
2024-03-06 18:15:53 -0600

    Creacion de Data Class Game

--- app/src/main/java/com/example/recyclerview/views/GameDataView.kt
+++ app/src/main/java/com/example/recyclerview/views/GameDataView.kt
@@ -1,2 +1,23 @@
 package com.example.recyclerview.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true)
+@Composable
+fun GameDataView(modificador : Modifier = Modifier.fillMaxWidth()){
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth()){
+        Text(text = "PS4")
+        Text(text = "1600 MXN")
+    }
+}
